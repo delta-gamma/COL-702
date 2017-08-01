43a82261d205efbb83abf5013b47e776e55825d1
delta-gamma <30531520+delta-gamma@users.noreply.github.com>
2017-08-01 09:47:10 +0530

    Update MergeKlistsUsingHeap.py

--- MergeKlistsUsingHeap.py
+++ MergeKlistsUsingHeap.py
@@ -1,22 +1,23 @@
+# Author: Mahima Manik
+# Date: 01-08-2017
 import random
 from heapq import heappush, heappop
 from random import randint
 
-number_of_lists = randint(0,5)
+number_of_lists = randint(0,50)     #number of lists is randomly generated number between 0 to 50
 random_data = []
 orderedL = []
 heap = []
 
-for number in range(0,number_of_lists):
-    list_length = randint(0,10)         # maximum list length is limited by 10
-    random_data.append(random.sample(range(1, 101), list_length))
+# maximum length of each list can be 100
+#random.sample returns a list of length list_length and appends it to random_data. So at the end of the for loop, random_data becomes list of lists
+for number in range(0, number_of_lists):
+    list_length = randint(0, 100)         
+    random_data.append(random.sample(range(1, 1001), list_length))  
 
+#sorting the data in the list
 for lists in range(0,len(random_data)):
     random_data[lists].sort()
-
-for lists in range(0,len(random_data)):
-    print random_data[lists]
-    print len(random_data[lists])
 
 for i in range(0,10):
     for lists in range(0, len(random_data)):
